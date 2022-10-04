16d5b25f90bc7bab40f91ae75c212167ac83bea5
yashi-garg <115034579+yashi-garg@users.noreply.github.com>
2022-10-04 23:37:56 +0530

    Add files via upload

---
+++ codeforces/codeforces_4A.cpp
@@ -0,0 +1,18 @@
+//codeforces 4A
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cin >> a;
+    if(a>2)
+    {
+    if(a%2==0)
+    cout << "YES";
+    else
+    cout << "NO";
+    }
+    else
+    cout << "NO";
+     return 0;
+}
