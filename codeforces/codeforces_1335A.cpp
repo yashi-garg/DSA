22ebc20e99c6a90911ebe1cb1278cc132eaa5fb2
yashi-garg <115034579+yashi-garg@users.noreply.github.com>
2022-12-07 18:25:41 +0530

    Add files via upload

---
+++ codeforces/codeforces_1335A.cpp
@@ -0,0 +1,15 @@
+//codeforces1335A
+#include<iostream>
+using namespace std;
+main()
+{
+	int n,i,x;
+	cin >> n;
+	for(i=0;i<n;i++)
+	{
+		cin >> x;
+		int c=(x-1)/2;
+		cout << c << endl;
+	}
+	
+	}
