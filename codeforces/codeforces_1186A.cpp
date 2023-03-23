2728e10b0fbd24def80054ad4f431627eff2e823
yashi-garg <115034579+yashi-garg@users.noreply.github.com>
2023-03-24 01:14:02 +0530

    Add files via upload

---
+++ codeforces/codeforces_1186A.cpp
@@ -0,0 +1,12 @@
+//codeforces1186A
+#include<iostream>
+using namespace std;
+ int main()
+ {
+     int n,m,k;
+     cin >> n>>m>>k;
+     if(m>=n && k>=n)
+     cout << "yes";
+     else
+     cout << "no";
+ }
