a40ba6157872e3074d166e6a4b6a0f27620c7314
yashi-garg <115034579+yashi-garg@users.noreply.github.com>
2023-03-26 02:21:14 +0530

    Add files via upload

---
+++ codeforces/codeforces_1624A.cpp
@@ -0,0 +1,23 @@
+//codeforces1624A
+#include<bits/stdc++.h>
+#include <iostream>
+#include<algorithm>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t-->0)
+    {
+        int n;
+        cin >> n;
+       int a[n];
+       for(int i=0;i<n;i++)
+       {
+           cin >> a[i];
+       }
+       sort(a,a+n);
+       cout << (a[n-1]-a[0])<<endl;
+
+    }
+}
