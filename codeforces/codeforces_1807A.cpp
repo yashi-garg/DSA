25967e677c534903dfe2f094578d6d13ea9b5e6b
yashi-garg <115034579+yashi-garg@users.noreply.github.com>
2023-03-26 18:32:03 +0530

    Add files via upload

---
+++ codeforces/codeforces_1807A.cpp
@@ -0,0 +1,17 @@
+//codeforces1870A
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t-->0)
+    {
+        int a,b,c;
+        cin >> a>> b >> c;
+        if(a-b==c)
+        cout << "-"<<endl;
+        else if(a+b==c)
+        cout << "+"<<endl;
+    }
+}
